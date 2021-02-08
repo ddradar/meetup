bc9f1b763b2e42a32efb6591858f1116bb0d45ab
nogic <24802730+nogic1008@users.noreply.github.com>
2021-02-08 21:50:41 +0900

    test: remove test case

--- functions/__tests__/v1/firestore/missions.test.ts
+++ functions/__tests__/v1/firestore/missions.test.ts
@@ -56,31 +56,23 @@ describe('Function v1-firestore-missions', () => {
       }
     )
 
-    test.each([
-      [{ cleared: false }, { cleared: true }],
-      [{ cleared: true }, { cleared: true }],
-    ])(
-      'resets user.orderedMission if change from %p to %p',
-      async (prevData, newData) => {
-        // Arrange
-        const db = provider.getAdminFirestore()
-        const expected = { ...user, orderedMission: missionId }
-        await db
-          .doc(`/version/1/missions/${missionId}`)
-          .set({ ...mission, ...prevData })
-        await db.doc(`/version/1/users/${userId}`).set(expected)
+    test('resets user.orderedMission if change from {cleared: false} to {cleared: true}', async () => {
+      // Arrange
+      const db = provider.getAdminFirestore()
+      const expected = { ...user, orderedMission: missionId }
+      await db.doc(`/version/1/missions/${missionId}`).set({ ...mission })
+      await db.doc(`/version/1/users/${userId}`).set(expected)
 
-        // Act
-        await db
-          .doc(`/version/1/missions/${missionId}`)
-          .set(newData, { merge: true })
-        await sleep() // Wait for Function
+      // Act
+      await db
+        .doc(`/version/1/missions/${missionId}`)
+        .set({ ...mission, cleared: true }, { merge: true })
+      await sleep() // Wait for Function
 
-        // Assert
-        const result = await db.doc(`/version/1/users/${userId}`).get()
-        expect(result.data()).toStrictEqual(user)
-      }
-    )
+      // Assert
+      const result = await db.doc(`/version/1/users/${userId}`).get()
+      expect(result.data()).toStrictEqual(user)
+    })
 
     test('does not reset user.orderedMission if other mission', async () => {
       // Arrange
