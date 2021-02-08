7c12146368bea71162fc488370025f32d1910e76
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-02-08 15:08:31 +0900

    feat(func): add logger & timeout

--- functions/v1/firestore/missions.ts
+++ functions/v1/firestore/missions.ts
@@ -10,16 +10,23 @@ export const onUpdate = functions
   .firestore.document('/version/1/missions/{missionID}')
   .onUpdate(async (snapshot, context) => {
     const newData = snapshot.after.data() as MissionListData
+    const missionID = context.params.missionID
 
-    if (newData.cleared) {
-      const users = await db
-        .collection('/version/1/users')
-        .where('orderedMission', '==', context.params.missionID)
-        .get()
-      const batch = db.batch()
-      users.docs.forEach(doc =>
-        batch.set(doc.ref, { orderedMission: null }, { merge: true })
-      )
-      await batch.commit()
+    if (!newData.cleared) {
+      functions.logger.info(`/version/1/missions/${missionID}.cleared is false. skipped.`)
+      return
     }
+
+    functions.logger.info(`/version/1/missions/${missionID}.cleared is true. start to update.`)
+    const users = await db
+      .collection('/version/1/users')
+      .where('orderedMission', '==', missionID)
+      .get()
+    const batch = db.batch()
+    users.docs.forEach(doc => {
+      functions.logger.info(`Update /version/1/users/${doc.id}`)
+      batch.set(doc.ref, { orderedMission: null }, { merge: true })
+    })
+    await batch.commit()
+    functions.logger.info('Update Finished.')
   })
