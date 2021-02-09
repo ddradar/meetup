54d6ff858ff78aa31c07fa7e014cd7db04806bdf
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-02-09 13:45:50 +0900

    feat(client): add root store

---
+++ client/store/index.ts
@@ -0,0 +1,43 @@
+import type firebase from 'firebase'
+import {
+  actionTree,
+  getAccessorType,
+  getterTree,
+  mutationTree,
+} from 'nuxt-typed-vuex'
+
+export const state = () => {
+  return { user: null as firebase.User | null }
+}
+
+export const getters = getterTree(state, {
+  isLoggedIn: state => !!state.user,
+})
+
+export const mutations = mutationTree(state, {
+  setUser(state, user: firebase.User | null): void {
+    state.user = user
+  },
+})
+
+export const actions = actionTree(
+  { state, getters, mutations },
+  {
+    async login({ commit }) {
+      const provider = new this.$fireModule.auth.TwitterAuthProvider()
+      const result = await this.$fire.auth.signInWithPopup(provider)
+      commit('setUser', result.user)
+    },
+    async logout({ commit }) {
+      await this.$fire.auth.signOut()
+      commit('setUser', null)
+    },
+  }
+)
+
+export const accessorType = getAccessorType({
+  state,
+  getters,
+  mutations,
+  actions,
+})
